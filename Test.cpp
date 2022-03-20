0880fea633aa12b282f188a4768ebf7392c97dd4
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-20 21:40:28 +0200

    changed sme of the tests

--- Test.cpp
+++ Test.cpp
@@ -24,12 +24,12 @@ TEST_CASE("good write and read"){
     CHECK(notebook2.read(/*page*/0,  /*row*/0,  /*column*/0,  Direction::Horizontal,10)=="cpp is fun");
     
     notebook1.write(/*page*/0,/*row*/1, /*column*/1,Direction::Vertical,"ABCD");
-    CHECK(notebook1.read(/*page*/0,  /*row*/0,  /*column*/0,  Direction::Vertical,4)=="ABCD");
+    CHECK(notebook1.read(/*page*/0,  /*row*/1,  /*column*/1,  Direction::Vertical,4)=="ABCD");
 
-}
+ }
 
 TEST_CASE("bad write"){
-    CHECK_THROWS(notebook1.write(/*page*/0,  /*row*/0,  /*column*/20,  Direction::Horizontal,"cpp is fun"));  // cant write over somthing written
+    CHECK_THROWS(notebook1.write(/*page*/0,  /*row*/0,  /*column*/10,  Direction::Horizontal,"cpp is fun"));  // cant write over somthing written
     CHECK_THROWS(notebook1.write(/*page*/0,  /*row*/4,  /*column*/1,  Direction::Horizontal,"DEFG"));  // cant write over somthing written
     CHECK_THROWS(notebook1.write(/*page*/0,  /*row*/0,  /*column*/95,  Direction::Horizontal,"cpp is fun"));  //goes out of bounds in the end of the row  
     // CHECK_THROWS(notebook1.write(/*page*/-1,  /*row*/0,  /*column*/20,  Direction::Horizontal,"cpp is fun"));  // not allowed page number
@@ -52,7 +52,7 @@ TEST_CASE("bad read1"){
 }
 
 
-// tests for erase
+//tests for erase
 
 TEST_CASE("good erase and read"){
     notebook1.erase(/*page*/0,  /*row*/0,  /*column*/0,  Direction::Horizontal,11);
@@ -61,11 +61,11 @@ TEST_CASE("good erase and read"){
     notebook1.erase(/*page*/0,/*row*/1, /*column*/1,Direction::Vertical,4);   
     notebook1.erase(/*page*/0,/*row*/3, /*column*/0,Direction::Horizontal,4);
 
-    CHECK(notebook1.read(/*page*/0,  /*row*/0,  /*column*/0,  Direction::Vertical,5)=="~~~~~"); // erased hello world from row 1
-    CHECK(notebook1.read(/*page*/0,  /*row*/1,  /*column*/0,  Direction::Vertical,5)=="_~___"); // erased ABCD from column 1
-    CHECK(notebook1.read(/*page*/0,  /*row*/2,  /*column*/0,  Direction::Vertical,5)=="_~___"); // erased ABCD from column 1
-    CHECK(notebook1.read(/*page*/0,  /*row*/3,  /*column*/0,  Direction::Vertical,5)=="~~~~_"); // erased ABCD from column 1 and the first for places in row 3
-    CHECK(notebook1.read(/*page*/0,  /*row*/4,  /*column*/0,  Direction::Vertical,5)=="_~___"); // erased ABCD from column 1
+    CHECK(notebook1.read(/*page*/0,  /*row*/0,  /*column*/0,  Direction::Horizontal,5)=="~~~~~"); // erased hello world from row 1
+    CHECK(notebook1.read(/*page*/0,  /*row*/1,  /*column*/0,  Direction::Horizontal,5)=="_~___"); // erased ABCD from column 1
+    CHECK(notebook1.read(/*page*/0,  /*row*/2,  /*column*/0,  Direction::Horizontal,5)=="_~___"); // erased ABCD from column 1
+    CHECK(notebook1.read(/*page*/0,  /*row*/3,  /*column*/0,  Direction::Horizontal,5)=="~~~~_"); // erased ABCD from column 1 and the first for places in row 3
+    CHECK(notebook1.read(/*page*/0,  /*row*/4,  /*column*/0,  Direction::Horizontal,5)=="_~___"); // erased ABCD from column 1
 }
 
 TEST_CASE("bad erase"){
@@ -84,10 +84,10 @@ TEST_CASE("loop1"){
     for (unsigned int i=0; i<110;i++){
         if(i<=88){
             notebook3.write(/*page*/i,  /*row*/i,  /*column*/i,  Direction::Horizontal,"hello world");
-            CHECK(notebook1.read(/*page*/i,  /*row*/i,  /*column*/i,  Direction::Horizontal,11)=="hello world"); 
+            CHECK(notebook3.read(/*page*/i,  /*row*/i,  /*column*/i,  Direction::Horizontal,11)=="hello world"); 
         }
         else{
-            CHECK_THROWS(notebook1.write(/*page*/i,  /*row*/i,  /*column*/i,  Direction::Horizontal,"hello world")); // the words become out of bounds or start out of bounds
+            CHECK_THROWS(notebook3.write(/*page*/i,  /*row*/i,  /*column*/i,  Direction::Horizontal,"hello world")); // the words become out of bounds or start out of bounds
         }
     }
 }
